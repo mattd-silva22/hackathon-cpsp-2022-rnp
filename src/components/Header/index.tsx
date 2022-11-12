7a9bb6c0033f1208c2c6a204978825a79ffb2f08
roymas100 <amir.dantas@hotmail.com>
2022-11-12 16:45:46 -0300

    add:compoenent-folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header: React.FC = () => {
+  return <div />;
+};
+
+export default Header;
+
+export const Container = styled.div``;

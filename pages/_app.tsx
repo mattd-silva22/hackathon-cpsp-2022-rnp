d89839884c9281571280fdec0cfe2c7438ab1666
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2022-11-12 21:32:47 -0300

    add global styled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AppProvider from "../src/hooks/AppProvider";
+import { GlobalStyled } from "../src/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
+      <GlobalStyled/>
       <Component {...pageProps} />
     </AppProvider>
   );

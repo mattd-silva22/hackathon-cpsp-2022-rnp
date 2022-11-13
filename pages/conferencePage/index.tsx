c5b5c42ef70ca9f16fba485d85e42581814118ae
roymas100 <amir.dantas@hotmail.com>
2022-11-13 00:45:23 -0300

    add:chat-tab

--- pages/conferencePage/index.tsx
+++ pages/conferencePage/index.tsx
@@ -1,48 +1,22 @@
-
 import React from "react";
 import styled from "styled-components";
 import VideoContent from "../../src/components/VideoContent";
 
-import BarraAcessibilidade from '../../src/components/BarraAcessibilidade'
-
+import BarraAcessibilidade from "../../src/components/BarraAcessibilidade";
+import ChatTab from "../../src/components/ChatTab";
 
 export default function ConferencePage() {
   return (
     <ConferencePageContainer>
-        <ConferencePageContent>
-            <main className="conference-area">
+      <ConferencePageContent>
+        <main className="conference-area">
+          <BarraAcessibilidade />
 
-                <BarraAcessibilidade/>
+          <VideoContent />
+        </main>
 
-                <header className="conference-room-header">
-                    <h1> Lorem ipsum dolor sit amet consectetur| room 312 </h1>
-                </header>
-
-
-                <section className="conference-room-section">
-
-                    <nav className="conferece-room-controller-top">
-                        oi 2 
-                    </nav>
-
-                    <video src="">
-                        video aqui
-                    </video>
-
-                    <nav className='conference-controller-menu'>
-                        oi
-                    </nav>
-
-                </section>
-
-            </main>
-
-            <section className="aside-menu">
-                <h1>
-                    oi lado
-                </h1>
-            </section>
-        </ConferencePageContent>
+        <ChatTab />
+      </ConferencePageContent>
     </ConferencePageContainer>
   );
 }
@@ -53,23 +27,18 @@ const ConferencePageContainer = styled.div`
   justify-content: center;
   width: 100%;
   background-color: #f4f4f5;
+  padding: 32px;
 `;
 
 const ConferencePageContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 95%;
+  gap: 16px;
 
   border: 1px solid blue;
+  max-width: 100%;
 
-  main {
-    width: 100%;
-  }
-
-  
-
-  .aside-menu {
-    background-color: blue;
+  .conference-area {
   }
 `;
---
+++ src/components/Message/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import * as Avatar from "@radix-ui/react-avatar";
+import { useSpeechSynthesis } from "react-speech-kit";
+
+interface MessageProps {
+  text: string;
+  imgUrl: string;
+  userName: string;
+}
+
+const Message: React.FC<MessageProps> = ({ text, imgUrl, userName }) => {
+  const { speak } = useSpeechSynthesis();
+
+  return (
+    <Container>
+      <Avatar.Root className="AvatarRoot">
+        <Avatar.Image className="AvatarImage" src={imgUrl} alt="Colm Tuite" />
+        <Avatar.Fallback className="AvatarFallback" delayMs={600}>
+          {userName[0].toLocaleUpperCase()}
+        </Avatar.Fallback>
+      </Avatar.Root>
+
+      <button
+        className="message-text"
+        onClick={() => speak({ text: userName + "disse: - " + text })}
+      >
+        {text}
+      </button>
+    </Container>
+  );
+};
+
+export default Message;
+
+export const Container = styled.div`
+  display: flex;
+  gap: 8px;
+
+  .AvatarImage {
+    width: 45px;
+    height: 45px;
+    border-radius: 100%;
+  }
+
+  .message-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+
+    width: 100%;
+
+    background: ${({ theme }) => theme.colors.base150};
+    border-radius: 8px;
+  }
+`;

e80dc717f36156cb9dd82deb955a07f035cfe0cc
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2022-11-13 18:52:16 -0300

    fix build error

--- pages/conferencePage/index.tsx
+++ pages/conferencePage/index.tsx
@@ -4,8 +4,44 @@ import VideoContent from "../../src/components/VideoContent";
 
 import BarraAcessibilidade from "../../src/components/BarraAcessibilidade";
 import ChatTab from "../../src/components/ChatTab";
-import { ConferencePageContainer, ConferencePageContent } from "./styles";
 import VideoController from "../../src/components/VideoControllers";
+import styled from "styled-components";
+
+export const ConferencePageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.base150};
+  padding: 32px;
+`;
+
+export const ConferencePageContent = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  width: 100%;
+  max-width: 100%;
+
+  .conference-area {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-direction: column;
+    gap: 16px;
+    width: 70%;
+  }
+
+  .video-area {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    flex-direction: column;
+    gap: 40px;
+  }
+`;
+
 
 export default function ConferencePage() {
   return (
@@ -24,3 +60,5 @@ export default function ConferencePage() {
     </ConferencePageContainer>
   );
 }
+
+
